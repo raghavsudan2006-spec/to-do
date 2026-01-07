62cb57b1f9b4750f55893970487cf2b80e973b3d
raghav sharma <raghavsudan2006@gmail.com>
2026-01-07 21:06:16 +0530

    Problem - When use read mode then data get automatically deleted from the file

--- to-do.py
+++ to-do.py
@@ -4,13 +4,23 @@ data1 = []
 def add():
     while True:
         task = input("Entre the task:")
+        with open("to-do.json","r")as f:
+            data = json.load(f)
+         
+        print(type(data))
 
         print(data1)
         data1.append({
+            "tasks":[{
             "task":task,
             "Mode" : "Not complete"
+            }
+            ]
         })
-        with open("to-do.json","a")as f:
+
+        
+
+        with open("to-do.json","w")as f:
             json.dump(data1,f,indent = 3)
 
         query = input("Do you want to add another task:")
