1aa234f6563631c355fa38e5ae5e5d12c23550df
raghav sharma <raghavsudan2006@gmail.com>
2026-01-09 19:19:42 +0530

    Add read mode and try-except for first task

--- to-do.py
+++ to-do.py
@@ -4,13 +4,20 @@ data1 = []
 def add():
     while True:
         task = input("Entre the task:")
-        with open("to-do.json","r")as f:
-            data = json.load(f)
-         
-        print(type(data))
 
-        print(data1)
         data1.append({
+            "tasks":[{
+            "task":task,
+            "Mode" : "Not complete"
+            }
+            ]
+        })
+
+        try:
+            with open("to-do.json","r")as f:
+                 data = json.load(f)
+
+            data1.append({
             "tasks":[{
             "task":task,
             "Mode" : "Not complete"
@@ -20,15 +27,44 @@ def add():
 
         
 
-        with open("to-do.json","w")as f:
-            json.dump(data1,f,indent = 3)
-
+            with open("to-do.json","w")as f:
+                 json.dump(data1,f,indent = 3)
+        
+            query = input("Do you want to add another task:")
+            if(query.lower() == "yes"):
+               continue
+            else:
+               print("Greetings")
+               break
+        except:
+            with open("to-do.json","w")as f:
+                 json.dump(data1,f,indent = 3)
         query = input("Do you want to add another task:")
         if(query.lower() == "yes"):
-            continue
+               continue
         else:
-            print("Greetings")
-            break
+               print("Greetings")
+               break
+            
+        # data1.append({
+        #     "tasks":[{
+        #     "task":task,
+        #     "Mode" : "Not complete"
+        #     }
+        #     ]
+        # })
+
+        
+
+        # with open("to-do.json","w")as f:
+        #     json.dump(data1,f,indent = 3)
+        
+        # query = input("Do you want to add another task:")
+        # if(query.lower() == "yes"):
+        #     continue
+        # else:
+        #     print("Greetings")
+        #     break
 
     
 ques = input("Which task you want to perform:")
