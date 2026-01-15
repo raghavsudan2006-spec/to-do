c18dba73ff20c3f3c6a394053a72ccdf2be0918e
raghav sharma <raghavsudan2006@gmail.com>
2026-01-15 14:39:08 +0530

    Modify change more next time

--- to-do.py
+++ to-do.py
@@ -49,26 +49,45 @@ def view():
     for ln in data["todo"]:
         print(f"{ln["id"]}. {ln["task"]} -> {ln["mode"]}")
 
-def delete():
-    flag = 0
-    ques = input("Which task you want to perform(delete or change status):")
-    if(ques.lower() == "delete" ):
-        que = input("Entre the task you want to delete:")
-    
+def change():
+    while True:
+        flag = 1
+        que = input("Entre the task which is done:")
         with open("to-do.json","r")as f:
             data = json.load(f)
-        for ln in data["todo"]:
-            if(ln["task"] == que.lower()):
-               flag = 1
-               break
-        if(flag == 1):
-            for ln in data["todo"]:
-              if(ln["task"].lower() == que.lower()):
-                ID  = ln["id"]
-                break
-            # for ln in data["todo"][ID:]:
-                
 
+        for ln in data["todo"]:
+            if(ln["task"].lower() == que.lower()):  
+                ID = ln["id"]
+                flag = 0
+                qu = input("Do you want to delete the changed task:")
+                if(qu.lower() == "yes"):
+                    data["todo"].remove(ln)
+                else:
+                    ln["status"] = "completed"
+                break
+               
+        if(flag == 1):
+            print("The task does not exist")
+            q = input("Do you want to change anything:")
+            if(q.lower() == "yes"):
+                continue
+            else:
+                print("Greetings")
+                break
+        for ln in data["todo"]:
+            if(ln["id"]>ID):
+                ln["id"] == ln["id"] - 1
+
+        with open("to-do.json","w")as f:
+            json.dump(data,f,indent = 3)
+
+        q = input("Do you want to change anything:")
+        if(q.lower() == "yes"):
+                continue
+        else:
+                print("Greetings")
+                break
 
 ques = input("Which task you want to perform:")
 if(ques.lower() == "add"):
@@ -76,4 +95,4 @@ if(ques.lower() == "add"):
 elif(ques.lower() == "view"):
     view()
 elif(ques.lower() == "change"):
-    delete()
+    change()
